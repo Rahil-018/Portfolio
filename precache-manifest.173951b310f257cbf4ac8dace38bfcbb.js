f3553941a5bbd334ec2e1dbe8f82f3f6ddf96486
Rahil-018 <rahil654khan@gmail.com>
2023-08-27 21:49:35 +0530

    Updates

---
+++ precache-manifest.173951b310f257cbf4ac8dace38bfcbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7b99054d966df15a562",
+    "url": "/Portfolio/static/css/main.a4a6d8b1.chunk.css"
+  },
+  {
+    "revision": "f7b99054d966df15a562",
+    "url": "/Portfolio/static/js/main.f7b99054.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/Portfolio/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "0d96e1bcadd6e8f8aa33",
+    "url": "/Portfolio/static/css/2.f74ea0ba.chunk.css"
+  },
+  {
+    "revision": "0d96e1bcadd6e8f8aa33",
+    "url": "/Portfolio/static/js/2.0d96e1bc.chunk.js"
+  },
+  {
+    "revision": "36f96fa61ec5e9db6ff8",
+    "url": "/Portfolio/static/js/runtime~main.36f96fa6.js"
+  },
+  {
+    "revision": "92ad07234a524f67dc78c79b03ac80c9",
+    "url": "/Portfolio/index.html"
+  }
+];
